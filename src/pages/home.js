eb4e2d6a85ab7de2fd9986fbee4f1989fe45ca63
Beck Millet <beck.millet@beck.millet>
2021-03-22 13:41:20 -0700

    Delay update

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,11 @@ import './home.css';
 export default function Home() {
     return (
         <div className='main'>
-            <p className='pp'>wassup</p>
+            <p className='pp'>*** Site under construction, delays due to working with legal team to share company work ***</p>
+            <p className='ppp'>If interested, please reach out to discuss past work, hold a portfolio review, etc.</p>
             <div className='picWindow'></div>
-            <p className='pp'>wassup</p>
-            <p className='pp'>wassup</p>
+            <p className='pp'></p>
+            <p className='pp'></p>
         </div>
     )
 }
