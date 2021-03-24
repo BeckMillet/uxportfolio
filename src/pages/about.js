7904fcf716fee350f941ac0959bdbc88df8a7118
Beck Millet <beck.millet@beck.millet>
2021-03-24 14:43:05 -0700

    rebrand

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,9 @@ import './about.css';
 export default function About() {
     return (
         <div>
-            
+            <p className='ppp'></p>
+            <div className='picWindow'></div>
+            <p className='ppp'></p>
         </div>
     )
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,10 @@ import './home.css';
 export default function Home() {
     return (
         <div className='main'>
-            <p className='pp'>*** Site under construction, delays due to working with legal team to share company work ***</p>
-            <p className='ppp'>If interested, please reach out to discuss past work, hold a portfolio review, etc.</p>
-            <div className='picWindow'></div>
             <p className='pp'></p>
+            <p className='pp'>*** Site under construction, delays due to working with legal team to share company work ***</p>
+            <p className='pp'>If interested, please reach out to discuss past work, hold a portfolio review, etc.</p>
+            <p className='picWindow'></p>
             <p className='pp'></p>
         </div>
     )
