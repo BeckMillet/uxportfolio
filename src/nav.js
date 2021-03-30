f14975bd90bfe263bc314550b5c9109870018497
Beck Millet <beck.millet@beck.millet>
2021-03-30 13:48:20 -0700

    OS 0

--- src/nav.js
+++ src/nav.js
@@ -4,17 +4,17 @@ export default function Nav() {
 
     const location = useLocation();
     const buttons = [
-      { path: '/home', label: 'Home', icon: '#icon-home'},
-      { path: '/research', label: 'UX Research', icon: '#icon-presentation'},
-      { path: '/dev', label: 'Development', icon: '#icon-laptop'},
-      { path: '/about', label: 'About Me', icon: '#icon-person-male'},
+      { path: 'home', label: 'Home', icon: '#icon-home'},
+      { path: 'research', label: 'UX Research', icon: '#icon-presentation'},
+      { path: 'dev', label: 'Development', icon: '#icon-laptop'},
+      { path: 'about', label: 'About Me', icon: '#icon-person-male'},
     ]
 
     return (
         <div className='nav'>
             <div className='navtext navtitle'>Portfolio</div>
           {buttons.map((button) => {
-            const className = button.path == location.pathname ? 'navbuttons active' : 'navbuttons';
+            const className = '/' + button.path === location.pathname ? 'navbuttons active' : 'navbuttons';
             return (
                 <Link to={button.path} className='navtext'>
                     <div className={className}>
